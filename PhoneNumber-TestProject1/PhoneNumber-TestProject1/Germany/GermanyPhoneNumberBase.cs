0683c70d1b97b67545bb46815825c7ab7bb7a041
eejai42 <ej@ssot.me>
2022-07-11 01:05:47 -0500

    bump.

---
+++ PhoneNumber-TestProject1/PhoneNumber-TestProject1/Germany/GermanyPhoneNumberBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneNumber_TestProject1 
+{
+    public abstract partial class GermanyPhoneNumberBase : PhoneNumberBase
+    {
+        protected GermanyPhoneNumberBase(string countryName, string e164Format, int expectedLength)
+            : base(countryName, e164Format, expectedLength)
+        {
+        }
+    }
+}
